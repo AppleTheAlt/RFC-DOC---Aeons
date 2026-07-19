4ddd2250be865da2229a311483ba33c3dd8abf1d
RFC Aeons <applethealt@gmail.com>
2026-07-19 01:49:25 +0100

    Update Contrib for 1.4.1

--- Assets/Python/Contrib/Civ4lerts.py
+++ Assets/Python/Contrib/Civ4lerts.py
@@ -923,15 +923,11 @@ class SlaveTrade(AbstractStatefulAlert):
 	def onBeginActivePlayerTurn(self, argsList):
 		if not Civ4lertsOpt.isShowSlaveTradeAlert():
 			return
-		
 		if len(self.maxSlaveTrade) == 0:
 			return
-		
 		playerID = PlayerUtil.getActivePlayerID()
-		
 		if not gc.getPlayer(playerID).canBuySlaves():
-			return
-		
+ 			return
 		for rival in TradeUtil.getSlaveTradePartners(playerID):
 			rivalID = rival.getID()
 			oldMaxSlaveTrade = self._getMaxSlaveTrade(playerID, rivalID)
